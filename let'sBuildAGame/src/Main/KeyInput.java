97490aa4a0c54e696d3a2112a97ecded321f9f9c
Osku Sirpoma <osku.sirpoma@gmail.com>
2017-09-27 01:40:05 +0300

    -

--- let'sBuildAGame/src/Main/KeyInput.java
+++ let'sBuildAGame/src/Main/KeyInput.java
@@ -22,45 +22,45 @@ public class KeyInput extends KeyAdapter {
 
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
-        
-        for(int i = 0; i < handler.object.size(); i++) {
+
+        for (int i = 0; i < handler.object.size(); i++) {
             GameObject tempObject = handler.object.get(i);
-            if(tempObject.getID() == ID.Player) {
-                
-                if(key == KeyEvent.VK_W) {
+            if (tempObject.getID() == ID.Player) {
+
+                if (key == KeyEvent.VK_W) {
                     tempObject.setVelY(-5);
                 }
-                if(key == KeyEvent.VK_S) {
+                if (key == KeyEvent.VK_S) {
                     tempObject.setVelY(5);
                 }
-                if(key == KeyEvent.VK_A) {
+                if (key == KeyEvent.VK_A) {
                     tempObject.setVelX(-5);
                 }
-                if(key == KeyEvent.VK_D) {
+                if (key == KeyEvent.VK_D) {
                     tempObject.setVelX(5);
                 }
             }
         }
-        
+
     }
 
     public void keyReleased(KeyEvent e) {
         int key = e.getKeyCode();
-        
-        for(int i = 0; i < handler.object.size(); i++) {
+
+        for (int i = 0; i < handler.object.size(); i++) {
             GameObject tempObject = handler.object.get(i);
-            if(tempObject.getID() == ID.Player) {
-                
-                if(key == KeyEvent.VK_W) {
+            if (tempObject.getID() == ID.Player) {
+
+                if (key == KeyEvent.VK_W) {
                     tempObject.setVelY(0);
                 }
-                if(key == KeyEvent.VK_S) {
+                if (key == KeyEvent.VK_S) {
                     tempObject.setVelY(0);
                 }
-                if(key == KeyEvent.VK_A) {
+                if (key == KeyEvent.VK_A) {
                     tempObject.setVelX(0);
                 }
-                if(key == KeyEvent.VK_D) {
+                if (key == KeyEvent.VK_D) {
                     tempObject.setVelX(0);
                 }
             }
