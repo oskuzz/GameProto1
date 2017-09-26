ea911c01cb7fb3c21d2482636c1962e844041242
Osku Sirpoma <osku.sirpoma@gmail.com>
2017-09-27 01:17:52 +0300

    -

---
+++ let'sBuildAGame/src/Main/Player.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
+
+/**
+ *
+ * @author osku0
+ */
+public class Player extends GameObject {
+
+
+    public Player(int x, int y, ID id) {
+        super(x, y, id);
+
+    }
+
+    public void tick() {
+        x += velX;
+        y += velY;
+    }
+
+    public void render(Graphics g) {
+        g.setColor(Color.white);
+        g.fillRect(x, y, 32, 32);
+    }
+
+}
